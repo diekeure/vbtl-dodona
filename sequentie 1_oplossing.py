5cf3a280ab160e5ec371bc5d198a6748db8fdd2f
katinkasteen <86676690+katinkasteen@users.noreply.github.com>
2021-07-06 14:30:40 +0200

    Add files via upload

---
+++ sequentie 1_oplossing.py
@@ -0,0 +1,14 @@
+zijde = float(input("Hoe lang is de zijde van het vierkant (in cm)? "))
+omtrek = 4 * zijde
+oppervlakte = zijde ** 2
+print("Omtrek van het vierkant:", omtrek, "cm")
+print("Oppervlakte van het vierkant:", oppervlakte, "cm²")
+
+
+
+
+
+
+
+
+
